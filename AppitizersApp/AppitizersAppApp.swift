46303581f8682d1a163f93435fee2a9df30edaf4
Sambhav Singh <ryugasingh90@gmail.com>
2024-11-05 16:52:46 +0530

    Initial Commit

---
+++ AppitizersApp/AppitizersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppitizersAppApp.swift
+//  AppitizersApp
+//
+//  Created by Sambhav Singh on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AppitizersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
